6f5f7fea54aadfe4ea617a641b0bf98e90aa48a8
Seu Nome <lucasfermagalhaes@gmail.com>
2026-01-17 22:11:00 -0300

    feat: adicionado mais uma camada de validações

--- src/main/java/Java10x/Itau/TransacaoService.java
+++ src/main/java/Java10x/Itau/TransacaoService.java
@@ -7,21 +7,26 @@ import java.time.OffsetDateTime;
 
 @Service
 public class TransacaoService {
-    public void validarTransacao(TransacaoDTO transacao){
+    public void validarTransacao(TransacaoDTO transacao) {
         //VALOR MAIOR OU IGUAL A ZERO
-        if (transacao.valor().compareTo(BigDecimal.ZERO) <0){
-            throw new IllegalArgumentException ("Erro: A transação deve tet um valor maior que zero");
-           //lança uma exceptional ao usuário de argumento inválido.
+        if (transacao.valor().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Erro: A transação deve ter um valor maior que zero");
+            //lança uma exceptional ao usuário de argumento inválido.
         }
 
         //DATA ATUAL OU MAIS ANTIGA
-        if (transacao.dataHora().isAfter(OffsetDateTime.now())){
+        if (transacao.dataHora().isAfter(OffsetDateTime.now())) {
             throw new IllegalArgumentException("Erro: A data da transação não é válida");
         }
 
-
-
         //DATA E HOR PRESENTES (BODY NÃO PODE SER VAZIO)
+        if (transacao.valor().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Erro: O valor digitado não é valido");
+        }
 
+
+        if (transacao.dataHora() == null){
+            throw new IllegalArgumentException("Erro: A data e a hora da transação não podem ser nulas.");
+        }
     }
 }
