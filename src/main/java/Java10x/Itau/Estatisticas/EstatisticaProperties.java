f104ad65dbd467a073933ac73bd37390f96c1100
Seu Nome <lucasfermagalhaes@gmail.com>
2026-01-26 15:47:43 -0300

    feat: criado a validação de tempo das operações.

--- src/main/java/Java10x/Itau/Estatisticas/EstatisticaProperties.java
+++ src/main/java/Java10x/Itau/Estatisticas/EstatisticaProperties.java
@@ -1,6 +1,8 @@
 package Java10x.Itau.Estatisticas;
 
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,14 @@ import org.springframework.stereotype.Component;
 
 
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer segundos) {
+public record EstatisticaProperties(
+        Integer segundos
+) {
+        public EstatisticaProperties{
+                if (segundos != 60) {
+                        throw new IllegalArgumentException("os segundos devem ser igual a 60");
+                }
+        }
+
 }
 
